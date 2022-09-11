38cbf4c9983b23a99de5108cdb4af3f67f3b6c3b
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-09-12 02:33:13 +0300

    added UITableViewDataSource extension in ChatViewController

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -15,11 +15,17 @@ class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hey"),
+        Message(sender: "nikita@com", body: "Hello!"),
+        Message(sender: "1@2.com", body: "what's up?")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = K.appName
         navigationItem.hidesBackButton = true
+        tableView.dataSource = self
+        
 
     }
     
@@ -41,3 +47,26 @@ class ChatViewController: UIViewController {
     }
     
 }
+
+extension ChatViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        return cell
+        
+        
+        
+    }
+    
+    
+}
+
+
+
+ 
+
+
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Nikita  on 9/12/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+
+struct Message{
+    let sender: String
+    let body: String
+    
+}
