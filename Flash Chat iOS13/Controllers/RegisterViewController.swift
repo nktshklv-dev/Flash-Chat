a36f0ce456c77c54f48af6b1871cd07495487bb1
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-09-11 14:24:12 +0300

    added new user registration and logging in

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseCore
+import FirebaseFirestore
 
 class RegisterViewController: UIViewController {
 
@@ -14,6 +17,22 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        guard let email = emailTextfield.text else {return}
+        guard let password = passwordTextfield.text else {return}
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            
+            guard error == nil else {
+                let ac = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(ac, animated: true, completion: nil)
+                return
+            }
+            
+            //navigate to the chat view controller
+            self.performSegue(withIdentifier: "FromRegisterToChat", sender: self)
+        }
     }
     
 }
